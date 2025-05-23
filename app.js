632f414bf4acbeb4a06b0dbcf96399f2476eb276
cn-gourav <mrgusain558@gmail.com>
2025-05-24 00:51:13 +0530

    ADD :: EXpressError handler

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsmate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync");
+const ExpressError = require("./utils/ExpressError");
 const Review = require("./models/review");
 
 app.set("views",path.join(__dirname,"views"));
@@ -30,10 +31,10 @@ app.get("/", (req, res) => {
 
 
 //Index route
-app.get("/listings", async (req, res) => {   
+app.get("/listings", wrapAsync( async (req, res) => {   
     const allListings = await Listing.find({});
     res.render("listings/index", {allListings }); 
-});
+}));
 
 // new form 
 app.get("/listings/new",(req,res)=>{
@@ -41,15 +42,20 @@ app.get("/listings/new",(req,res)=>{
 })
 
 // show route 
-app.get("/listings/:id", async(req,res)=>{
+app.get("/listings/:id",wrapAsync( async(req,res)=>{
   let{id} = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/show.ejs" , {listing});
-});
+}));
 
 // Create Route 
 app.post("/listings",wrapAsync(async(req,res)=>{
   // let {title,description,image,price,location,country} = req.body
+
+
+    if(!req.body.ExpressError){
+      throw new ExpressError(400,"Invalid Listing Data");
+    }
 
     let listing = req.body.listing;
     const newListing = new Listing(listing);
@@ -58,32 +64,43 @@ app.post("/listings",wrapAsync(async(req,res)=>{
 }));
 
 //edit and update 
-app.get("/listings/:id/edit" ,async(req,res)=>{
+app.get("/listings/:id/edit", wrapAsync(async(req,res)=>{
    let{id} = req.params;
   const listing = await Listing.findById(id);
   res.render("listings/edit" , {listing})
-})
+}))
 
 
 // update router
-app.put("/listings/:id" , async(req,res)=>{
+app.put("/listings/:id" , wrapAsync(async(req,res)=>{
+
+  
+  if(!req.body.ExpressError){
+    throw new ExpressError(400,"Invalid Listing Data");
+  }
   let {id} = req.params;
   await Listing.findByIdAndUpdate(id,{...req.body.listing});
   res.redirect(`/listings/${id}`)
-})
+}))
 
 // delete route 
-app.delete("/listings/:id" , async(req,res)=>{
+app.delete("/listings/:id" , wrapAsync( async(req,res)=>{
   let {id} = req.params;
   let deleting = await Listing.findByIdAndDelete(id);
   res.redirect("/listings")
-})
+}))
 
+app.all("*", (req, res, next) => {
+  next(new ExpressError("Page Not Found", 404));
+}); 
 
 // Error handle middleware 
 app.use((err,req,res,next)=>{
-  res.status(500).send("Something went wrong")
+  let{statusCode=500 , message = "Something want wrong"} = err;
+  res.status(statusCode).send(message);
 });
+
+
 
 //review route
 app.post("/listings/:id/reviews" , async(req,res)=>{
