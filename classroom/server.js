28410fda785e8bebd40887d8270aea6ce650ab50
cn-gourav <mrgusain558@gmail.com>
2025-05-25 23:46:43 +0530

    Create :: Connect Flash

--- classroom/server.js
+++ classroom/server.js
@@ -4,6 +4,11 @@ const users = require("./routes/user");
 const posts = require("./routes/post");
 const { reviewSchema } = require("../schema");
 const session = require("express-session");
+const flash =  require("connect-flash");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
 const sessionOptions = {
      secret: "mysecret",
@@ -12,16 +17,28 @@ const sessionOptions = {
 }
 
 app.use(session(sessionOptions));
+app.use(flash());
 
 app.get("/register", (req, res) => {
      let{name = "anoynmous"} = req.query;
      req.session.name = name;
      console.log(req.session.name);
+
+     if(name === "anoynmous") {
+          req.flash("error", "user registration failed");
+     }else{
+
+          req.flash("success", "user registered successfully");
+     }
      res.redirect("/login");
 });
 
 app.get("/login", (req, res) => {
-     res.send(`Hello ${req.session.name}, please login!`);
+     res.locals.successmsg = req.flash("success");
+     res.locals.errormsg = req.flash("error");
+
+     // console.log(req.flash("success"));
+     res.render("page.ejs",{name : req.session.name });
 })
 // app.get("reqcount", (req, res) => {
 //      if(req.session.count) {
