ffa456d479df94281141a17d6279c69072c6a194
cn-gourav <mrgusain558@gmail.com>
2025-05-25 15:12:25 +0530

    Restruction :: Review routes

--- app.js
+++ app.js
@@ -3,18 +3,14 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("./models/listing");
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsmate = require("ejs-mate");
-const wrapAsync = require("./utils/wrapAsync");
 const ExpressError = require("./utils/ExpressError");
-const {listingSchema} = require("./schema.js");
-const Review = require("./models/review");
-const {reviewSchema} = require("./schema.js");
 
-const listingRouter = require("./routes/listing");
+const listings = require("./routes/listing");
+const reviews = require("./routes/review");
 
 // Middleware
 app.set("views",path.join(__dirname,"views"));
@@ -34,56 +30,14 @@ async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-
-// vaildateReview middleware
-const vaildateReview = (req, res, next) => {
-  let {error} = reviewSchema.validate(req.body); // Validate the review data
-  if(error){
-    let errmsg = error.details.map((el) => el.message).join(", ");
-    throw new ExpressError(400, errmsg);
-  }else{
-    next();
-  }
-}
-
 // test router 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-
-
-
-
-//review route
-
-// post route 
-app.post("/listings/:id/reviews",vaildateReview,
-  wrapAsync(async(req,res)=>{
- let listing = await Listing.findById(req.params.id);
- let newReview = new Review(req.body.review);
- listing.reviews.push(newReview);
- await newReview.save();
- await listing.save();
- res.redirect(`/listings/${listing._id}`); 
-}))
-
-
-// delete review route
-app.delete("/lisitngs/:id/reviews/:reviewId", wrapAsync(async(req,res)=>{
-  let{id, reviewId} = req.params;
-  await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}}); // Remove review from listing
-
-  // Delete the review from the database
-  await Review.findByIdAndDelete(reviewId)
-
-  res.redirect(`/listings/${id}`);
-}))
-
-
 // All Router here !! 
-app.use("/listings", listingRouter);
-
+app.use("/listings", listings);
+app.use("/listings/:id/reviews", reviews)
  
 // all routes check 
 app.all("*", (req, res, next) => {
@@ -97,27 +51,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error.ejs" , {err});
 });
 
-
-// app.get("/testlisting", async (req, res) => {
-//   let sampleListing = new Listing({
-//     title: "t2est",
-//     description: "test",
-//     price: 2220,
-//     location: "test",
-//     country: "test",
-//   });
-
-//   await sampleListing
-//     .save()
-//     .then(() => {
-//       console.log("Listing saved");
-//       res.send("Listing saved");
-//     }) 
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
-
+// Start the server
 app.listen(8080, () => {
   console.log("Server is running on port 8080");
 });
---
+++ routes/review.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true }); // Merge params to access listing ID
+const wrapAsync = require('../utils/wrapAsync');
+const ExpressError = require('../utils/ExpressError');
+const Listing = require("../models/listing");
+const {listingSchema} = require("../schema.js");
+const { reviewSchema } = require('../schema.js');
+const Review = require('../models/review');
+
+
+
+// vaildateReview middleware
+const vaildateReview = (req, res, next) => {
+  let {error} = reviewSchema.validate(req.body); // Validate the review data
+  if(error){
+    let errmsg = error.details.map((el) => el.message).join(", ");
+    throw new ExpressError(400, errmsg);
+  }else{
+    next();
+  }
+}
+
+// post route 
+router.post("/",vaildateReview,
+  wrapAsync(async(req,res)=>{
+ let listing = await Listing.findById(req.params.id);
+ let newReview = new Review(req.body.review);
+ listing.reviews.push(newReview);
+ await newReview.save();
+ await listing.save();
+ res.redirect(`/listings/${listing._id}`); 
+}))
+
+
+// delete review route
+router.delete("/:reviewId", wrapAsync(async(req,res)=>{
+  let{id, reviewId} = req.params;
+  await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}}); // Remove review from listing
+
+  // Delete the review from the database
+  await Review.findByIdAndDelete(reviewId)
+
+  res.redirect(`/listings/${id}`);
+}))
+
+module.exports = router;
