e3322f4e21e1d541786f3e8bf449e3c2340b6d7d
cn-gourav <mrgusain558@gmail.com>
2025-05-27 23:44:01 +0530

    ADD :: login post

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router  = express.Router();
 const User = require('../models/user');
 const wrapAsync = require('../utils/wrapAsync');
+const passport = require('passport');
 
 router.get("/signup", (req, res) => {
      res.render("users/signup");
@@ -23,5 +24,21 @@ router.post("/signup", wrapAsync( async (req, res) => {
 
 }))
 
+router.get("/login", (req, res) => {
+     res.render("users/login");
+});
 
+
+router.post("/login",
+     passport.authenticate(
+          'local', 
+          {
+               failureRedirect: '/login',
+               failureFlash: true
+          }
+     )
+     ,async(req, res) => {
+          req.flash("success", "Welcome back!");
+          res.redirect("/listings");
+})
 module.exports = router;
